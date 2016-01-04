c9e29592d23951495cc05f58e8fe567aacfd6df8
Anton94 <antonvdudov@gmail.com>
2016-01-04 16:17:03 +0200

    Proper interface for the homework task (get number of coins of console and execute it

--- Homework two MinMaxAphaBetaCut/Homework two MinMaxAphaBetaCut/main.cpp
+++ Homework two MinMaxAphaBetaCut/Homework two MinMaxAphaBetaCut/main.cpp
@@ -8,19 +8,39 @@ int main()
 	_CrtMemState s1, s2, s3;
 	_CrtMemCheckpoint(&s1);
 	{
+		int n = 0;
+		std::cin >> n;
+		
+		if (n <= 0)
+		{
+			std::cout << "N is less than 1, wrong input!\n";
+			return 0;
+		}
+
+		CoinGame game(n);
+		game.generate(std::cout);
+
+		/*
 		std::ofstream out("output.txt");
-		for (int i = 1; i <= 12; ++i)
+		for (int i = 1; i <= 15; ++i)
 		{
 			CoinGame game(i);
 			game.generate(out);
 		}
-		for (int i = 1; i <= 12; ++i)
+		for (int i = 1; i <= 15; ++i)
 		{
 			CoinGame game(i);
 			game.generate(out, false);
 			game.printGameInfo(out);
 		}
-		out.close();
+		out.close();*/
+
+		//// Proper homework output design
+		//for (int i = 1; i <= 15; ++i)
+		//{
+		//	CoinGame game(i);
+		//	game.generate(std::cout);
+		//}
 	}
 
 	_CrtMemCheckpoint(&s2);
